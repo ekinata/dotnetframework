e43d4f4d97909937ae9d8a564930b75f1add9e59
ekinata <ekinatamuallaoglu@gmail.com>
2022-04-25 10:35:23 +0300

    frameworktest

---
+++ WpfApp1/WpfApp1/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// App.xaml etkileşim mantığı
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
